13bbaf865038ef1d7aa9fde6d1f945f99b85e99b
Aashraya Neupane <naashraya@gmail.com>
2023-07-28 08:42:34 +0545

    day 3

--- hangman.c
+++ hangman.c
@@ -5,8 +5,13 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_SCORE 5 // total wrong guesses allowed
+#define MAX_SCORE 7       // total wrong guesses allowed
 #define WORD_COUNT 1022
+
+const int hanger_x = 0;
+const int hanger_y = 3;
+const int parts_off[][2] = {{10, 3}, {10, 4}, {9, 4}, {11, 4}, {10, 5}, {9, 6}, {11, 6}};
+const char *parts_shape = "O|/\\|/\\";
 
 typedef struct {
   char *current_word;
@@ -14,7 +19,7 @@ typedef struct {
   size_t word_len;
   unsigned int score;
 
-  char *guesses;       // past guesses
+  char *guesses;          // past guesses
   unsigned int guesses_made;
   FILE *word_file;
 } Game;
@@ -65,12 +70,13 @@ void read_random_word(FILE *f, char **dst) {
   unsigned int index = (unsigned int)(((float)rand()) * (WORD_COUNT) / RAND_MAX);
   unsigned int count = 0;
 
+  fseek(f, 0, SEEK_SET);
   char ch;
   while (count < index) {
     ch = fgetc(f);
     if(ch == ',') count++;
   }
-  ch = fgetc(f);
+  if(index != 0) ch = fgetc(f);
 
   size_t word_len = 1;
   while ((ch = fgetc(f)) != ',') word_len++;
@@ -87,7 +93,7 @@ Game game_init(FILE *word_file) {
   game.word_file = word_file;
   read_random_word(game.word_file, &game.current_word);
   game.word_len = strlen(game.current_word);
-  game.display_word = (char*)malloc(game.word_len);
+  game.display_word = (char*)calloc(game.word_len, sizeof(char));
   for(int i = 0; i < game.word_len; ++i)
     game.display_word[i] = '_';
 
@@ -99,10 +105,7 @@ Game game_init(FILE *word_file) {
 }
 
 GameState game_update(Game *game, char in) {
-  if(str_contains(game->guesses, in)) {
-    printf("You have already guessed this.\n");
-    return GAME_RUNNING;
-  }
+  if(str_contains(game->guesses, in)) return GAME_RUNNING;
   game->guesses[game->guesses_made++] = in;
   
   int match = 0;
@@ -123,6 +126,12 @@ GameState game_update(Game *game, char in) {
   return GAME_RUNNING;
 }
 
+void game_destroy(Game *game) {
+  free(game->current_word);
+  free(game->display_word);
+  free(game->guesses);
+}
+
 char get_input() {
   char ch = 0;
   printf("Enter your guess: ");
@@ -138,6 +147,33 @@ char get_input() {
   return ch;
 }
 
+void draw(Game *game) {
+  clear_console();
+  printf("%s \t Score: %u \t Your guesses: ", game->display_word, game->score);
+  for(int i = 0; i < game->guesses_made; ++i)
+    printf("%c ", game->guesses[i]);
+  printf("\033[%d;%dH", hanger_y, hanger_x);
+  printf("---------+---------\n"
+	 "||       |\n"
+	 "||       |\n"
+	 "||\n"
+	 "||\n"
+	 "||\n"
+	 "||\n"
+	 "||\n"
+	 "||\n"
+	 "||\n"
+	 "||\n"
+	 "||_________________\n\n\n\n");
+
+  int parts_to_draw = MAX_SCORE - game->score;
+  for (int i = 0; i < parts_to_draw; ++i) {
+    printf("\033[%d;%dH", hanger_y + parts_off[i][1], hanger_x + parts_off[i][0]);
+    printf("%c", parts_shape[i]);
+  }
+  printf("\033[%d;%dH", hanger_y + 15, 0);
+}	 
+
 int main() {
   srand(time(0));
   FILE *file = fopen("words.txt", "r");
@@ -146,23 +182,26 @@ int main() {
     return 1;
   }
 
-  char *word = 0;
-  read_random_word(file, &word);
-  printf("%s\n", word);
+  char choice;
+  do {
+    Game game = game_init(file);
+    GameState state = GAME_RUNNING;
+    
+    while(state == GAME_RUNNING) {
+      draw(&game);
+      char in = get_input();
+      state = game_update(&game, in);
+    }
 
-  Game game = game_init(file);
-  GameState state = GAME_RUNNING;
+    draw(&game);
+    if(state == GAME_OVER) printf("You lost. The word was %s\n", game.current_word);
+    else printf("You won!! Score: %u\n", game.score);
 
-  while(state == GAME_RUNNING) {
-    clear_console();
-    printf("%s \t Score: %u\n", game.display_word, game.score);
-    char in = get_input();
-    state = game_update(&game, in);
-  }
-
-  clear_console();
-  if(state == GAME_OVER) printf("You lost. The word was %s\n", game.current_word);
-  else printf("You won!! Score: %u\n", game.score);
+    printf("Play again (y or n)? ");
+    scanf("%c", &choice);
+    clear_stdin();
+    game_destroy(&game);
+  } while(char_lower(choice) == 'y');
 
   return 0;
 }
