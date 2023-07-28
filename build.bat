55638da36e4d5b0eee91f670155d0c9fe10de83c
Aashraya Neupane <naashraya@gmail.com>
2023-07-28 09:09:14 +0545

    done

--- build.bat
+++ build.bat
@@ -1,13 +1,4 @@
 @echo off
 
-where gcc >nul 2>nul
-if %ERRORLEVEL% neq 0 goto SkipBuild
-
 if not exist "build" mkdir build
 gcc -o build/hangman.exe hangman.c
-goto Finished
-
-:SkipBuild
-echo "gcc not found"
-
-:Finished
