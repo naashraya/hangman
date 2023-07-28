a7820f218b9355561d6fb901b6f6c46cff443116
Aashraya Neupane <naashraya@gmail.com>
2023-07-28 09:07:59 +0545

    hii

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-which gcc &>/dev/null
-if [ $? == 0 ]
-then
-    mkdir -p build
-    gcc -o build/hangman hangman.c
-else
-    echo "gcc not found"
-fi
+mkdir -p build
+gcc -o build/hangman hangman.c
